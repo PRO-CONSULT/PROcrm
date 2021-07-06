8a2bacd7a98394d781e31a1db6f3c80b38bdca3b
Otto Algreen <72853405+proconsult@users.noreply.github.com>
2021-07-06 18:58:34 +0200

    top guid designet
    Først gang med brug af kommentar og grid styring, retting for at kunne få rette toppen ind. farverne er der kun for at hjælpe mig

--- grafiktest.php
+++ grafiktest.php
@@ -1,6 +1,20 @@
 <center>
+<div class="top">
+   <div class="top-logo">PROcrm placeholder</div>
+   <div class="top-klokke"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9Q0La2c8ujEQT9xkH5HPqzlQwfYs6Isycd0gbznWbFbYOjCSPl0jZkEeNJudxSajo_OnLRJJM&usqp=CAc" style="height: 50px;"></div>
+   <div class="top-klokketal">test</div>
+   <div class="top-mail">test</div>
+   <div class="top-mailtal">test</div>
+   <div class="top-aux">test</div>
+   <div class="top-auxtal">test</div>
+   <div class="adminmenu">test</div>
+</div>
+<div class="Menu">
+
+</div>
+<br>
 <div class="main">
-  <span class="tr1">
-  </span>
+  <div class="tr">.
+  </div>
 </div>
 </center>
