6ef1292a0e5ea70fd58d94ae3280abd7a4273aa2
Otto Algreen <72853405+proconsult@users.noreply.github.com>
2021-08-10 18:09:33 +0200

    More grafik made
    More grafik made

--- grafiktest.php
+++ grafiktest.php
@@ -9,12 +9,27 @@
    <div class="top-auxtal">test</div>
    <div class="adminmenu">test</div>
 </div>
-<div class="Menu">
-
+<div class="menu">
+  <div class="menuunder1">OVERSIGT</div>
+  <div class="menuunder2">SØG</div>
+  <div class="menuunder3">KUNDER</div>
+  <div class="menuunder4">LEVERANDØR</div>
+  <div class="menuunder5">LAGER</div>
+  <div class="menuunder6">MAILS</div>
+  <div class="menuunder7">AUX</div>
+  <div class="menuunder8">.</div>
+  <div class="menuunder9">SØGEFELT</div>
 </div>
 <br>
 <div class="main">
-  <div class="tr">.
-  </div>
+  <div class="tr1" id="maintr1">.</div>
+  <div class="mellemrum">.</div>
+  <div class="tr2" id="maintr2">.</div>
+  <div class="mellemrum">.</div>
+  <div class="tr3" id="maintr3">.</div>
+  <div class="mellemrum">.</div>
+  <div class="tr4" id="maintr4">.</div>
+  <div class="mellemrum">.</div>
+  <div class="tr5" id="maintr5">.</div>
 </div>
 </center>
