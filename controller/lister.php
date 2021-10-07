5f7e8f84e922d60991bfc33a4393310e411cb221
Otto Algreen <72853405+proconsult@users.noreply.github.com>
2021-10-07 20:11:35 +0200

    Save Funktion update
    Make opdates to the save system, add button, add text + div.

--- controller/lister.php
+++ controller/lister.php
@@ -7,13 +7,24 @@ include '../controller/config.php';
 $html = '<button onclick="listehent(this.value)" value="';
 $html .= 'jobnrString';
 $html .= '" id="forlist">';
-$html .= 'jobnrString';
+$html .= 'jobnrString <br>';
+$html .= 'FirmaNavnString';
+
 $html .= '</button><br>';
     
 	$sqljobslist = "SELECT * FROM jobs where Flag = '$listtype' ORDER BY JobsID DESC";
 	$resultjobslist = $conn->query($sqljobslist);
-        while($row = $resultjobslist->fetch_assoc()) {$jobsidlist = $row["JobsID"];
+        while($row = $resultjobslist->fetch_assoc()) {$jobsidlist = $row["JobsID"]; $debitorid = $row["debitorid"];
+
+	$sqljobs = "SELECT * FROM debitor where DebitorID = $debitorid";
+	$resultjobs = $conn->query($sqljobs);
+        while($row = $resultjobs->fetch_assoc()) {$firma = $row["Firma"];}
+
+
+
+
                 $output = str_replace('jobnrString', $jobsidlist, $html);
+                $output = str_replace('FirmaNavnString', $firma, $output);
 		echo($output);
 }
 
--- controller/saveorder.php
+++ controller/saveorder.php
@@ -1,33 +1,36 @@
 <?php
 include '../controller/config.php';
-print_r($jobsid = $_GET['q']);
-print_r($revk = $_GET['z']);
-
+$jobsid = $_GET['q'];
+$revk = $_GET['z'];
+$PlusType = $_GET['a'];
 
 $sql = "INSERT INTO debitor (Firma)
 VALUES ('$jobsid')";
 
-if ($conn->query($sql) === TRUE) {
-  echo "New record created successfully";
-} else {
-  echo "Error: " . $sql . "<br>" . $conn->error;
-}
+$conn->query($sql);
 
 $sqljobs = "SELECT * FROM debitor where Firma = '$jobsid'";
 $resultjobs = $conn->query($sqljobs);
 while($row = $resultjobs->fetch_assoc()) {$debitorid = $row["DebitorID"];}
 
-
 $sql = "INSERT INTO jobs (debitorid,flag,revk)
-VALUES ($debitorid, 'inq','$revk')";
+VALUES ($debitorid, '$PlusType','$revk')";
 
 
 if ($conn->query($sql) === TRUE) {
-  echo "New record created successfully";
-} else {
-  echo "Error: " . $sql . "<br>" . $conn->error;
-}
 
+$sqljobs = "SELECT JobsID FROM jobs ORDER BY JobsID DESC LIMIT 1";
+$resultjobs = $conn->query($sqljobs);
+while($row = $resultjobs->fetch_assoc()) {$jobsid = $row["JobsID"];}
+
+?>
+
+<div style="display: inline-block; height: 100%; background-color: green; color: #fff; width: 100%; font-size: 20px;">Du har nu Oprettet: <?php echo $jobsid;?></div>
+<input id="SagNoFromSave" type="hidden" value="<?php echo $jobsid;?>">
+
+<?php
+  echo " ";
+}
 $conn->close();
 
 ?>
